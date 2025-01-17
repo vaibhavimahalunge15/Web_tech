449ce4aeb70c86af000f3420a5279481a5e9e7e5
Vaibhavi Mahalunge <mahalungevaibhavi3@gmail.com>
2025-01-17 11:38:16 +0530

    Delete EY-week3-main/RealTime-Collaboration-Tool-main directory

--- EY-week3-main/RealTime-Collaboration-Tool-main/Backend/models/Document.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const DocumentSchema = new mongoose.Schema({
-  title: { type: String, default: "Untitled Document" },
-  content: { type: String, default: "" },
-});
-
-module.exports = mongoose.model("Document", DocumentSchema);
